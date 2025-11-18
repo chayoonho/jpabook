e3add367596dbe8668c00caf6a79a6e4d85f79da
Yoonho Cha <0119hoya@naver.com>
2025-11-18 23:01:01 +0900

    [feat] 주석 추가

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -9,4 +9,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    /*JPA 스펙상 엔티티나 임베디드 타입( `@Embeddable` )은 자바 기본 생성자(default constructor)를 `public` 또는 `protected` 로
+     설정해야 한다. `public` 으로 두는 것 보다는 `protected` 로 설정하는 것이 그나마 더 안전하다.*/
+    protected Address() {}
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
