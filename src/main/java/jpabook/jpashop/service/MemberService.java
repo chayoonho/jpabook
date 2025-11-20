cb1ba85ae5404efd7f44c3c5752412a3405a47a2
Yoonho Cha <0119hoya@naver.com>
2025-11-20 15:09:09 +0900

    [feat] MemberService 구현

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -2,20 +2,25 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional(readOnly = true) // readonly = true -> 조회 시 성능 최적화
+@RequiredArgsConstructor
 public class MemberService {
 
-    @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     /*
     * 회원가입
     * */
+    @Transactional
     public Long join(Member member) {
         validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
@@ -32,6 +37,12 @@ public class MemberService {
     /*
     * 회원 전체 조회
     * */
+    public List<Member> findMember(){
+        return memberRepository.findAll();
+    }
 
-
+    @Transactional(readOnly = true) 
+    public Member findOne(long memberId) {
+        return memberRepository.findOne(memberId);
+    }
 }
