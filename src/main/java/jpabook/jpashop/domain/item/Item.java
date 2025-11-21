c4f18f3165e5b96dab52ab99903362eaf8a80559
Yoonho Cha <0119hoya@naver.com>
2025-11-21 22:41:52 +0900

    [feat] 1. stock 증가 로직 추가 2. stock 감소 로직 추가 남은 재고가 0보다 작으면 NotEnoughStockException

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -26,4 +26,24 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<Category>();
+
+    /*
+    * Business Login
+    * */
+
+    //  엔티티 자체가 해결할 수 있으면 엔티티에서 해결해야지 객체지향적 -> 도메인주도설계
+    
+    // stock 증가
+    public void addStock(int quantity){
+        this.stockQuantity =+ quantity;
+    }
+
+    // stock 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0 ){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
