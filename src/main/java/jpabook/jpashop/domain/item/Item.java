0e3f37a0cce2050774cb9a77c9267a249f2ddd45
Yoonho Cha <0119hoya@naver.com>
2025-11-21 22:48:56 +0900

    [comment] 주석 추가

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -31,8 +32,15 @@ public abstract class Item {
     * Business Login
     * */
 
-    //  엔티티 자체가 해결할 수 있으면 엔티티에서 해결해야지 객체지향적 -> 도메인주도설계
-    
+    /* 엔티티 자체가 해결할 수 있으면 엔티티에서 해결해야지 객체지향적,
+     * 데이터가 가지고 있는 쪽에 비지니스 로직을 넣자 -> 도메인주도설계
+    */
+
+
+    /*
+    * setter를 가지고 값을 변경하지 말고, 아래처럼 비지니스 로직을 사용해서 값을 변경(수정)해야함
+    */
+
     // stock 증가
     public void addStock(int quantity){
         this.stockQuantity =+ quantity;
